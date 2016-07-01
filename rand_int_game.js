4d08b30c351d97fc10a486a405f1d220ade285ce
Albert Reel <waxhoya@gmail.com>
2016-06-30 20:57:53 -0700

    Fixed incorrect numnber of guesses being shown

--- rand_int_game.js
+++ rand_int_game.js
@@ -4,9 +4,9 @@ function randGame() {
 	var randInt = Math.round(Math.random() * (high - low) + low);
 	console.log(randInt);
 		for (i=0; i<4; i++) {
-			var guessMe = prompt("Lets play a guessing game. Try and guess my number. It is between "+high+" and "+low+". What do you think my number is?: ","" );
+			var guessMe = prompt("Lets play a guessing game. Try and guess my number, you have "+(4-i)+" guesses left. It is between "+high+" and "+low+". What do you think my number is?: ","" );
 			if (guessMe == randInt) {
-				alert("You totally guessed my number: " +randInt+". Way to go "+userName+"! You still have "+i+" guesses left over!");
+				alert("You totally guessed my number: " +randInt+". Way to go "+userName+"! You still have "+(i-4)+" guesses left over!");
 				i +=4;
 			} else if (guessMe > randInt) {
 				alert("You guessed too high! Try again.");
