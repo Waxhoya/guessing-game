e14879446f35fd941735f8c72e26c0487fd0734a
Waxhoya <waxhoya@gmail.com>
2016-07-11 14:13:19 -0700

    Rewrote index and style. Standardized JS and added array of obj based Question DB.

--- trivia.js
+++ trivia.js
@@ -2,26 +2,32 @@
 
 function compareENator(userInput) {
 	userInput == userInput.toLowerCase();
-	return userInput.atChar(0)
+	return userInput.charAt(0);
 }
 
-function getTrivia () {
+function getTrivia() {
+	msg="";
+	for (var index = 0; index < triviaBox.length; index++){
+		var answer = prompt("Question "+(index + 1)+". "+triviaBox[index].question+"\n  Yes/No");
+		console.log("Answer: "+answer+"| Trivia Q: "+triviaBox[index].question)
+		if (compareENator(answer) == triviaBox[index].ans) {
+			alert("Holey Rusted Metal Batman, You are Correct");
+			msg += "<p class='success'>You are Correct! You got number "+index+" right."
+			document.getElementById('resp').innerHTML = msg;
+		} else {
+			alert("No, Thats not correct");
+			msg += "<p class='failure'>You are Incorrect! You got number "+index+" wrong."
+			document.getElementById('resp').innerHTML = msg;
+		}
+	}
+}
 
-}
-var gameIts = '';
-var triviaBox = {
-	q1: 'Do I like bacon',
-	a1: 'y'
-	q2: 'Do I like Mustard',
-	a2: 'n'
-	q3: 'I like dogs',
-	a3: 'y'
-	q4: 'Do I like cats',
-	a4: 'y'
-	q5: 'Do I like skydiving',
-	a5: 'y'
-	q6: 'Do I like Orange is the new black?',
-	a6: 'n'
-	q7: 'Do I have a peg leg?',
-	a7: 'n'
-}
+
+var triviaBox = [
+	{question: "Soccer is your favorite sport", ans: "y"},
+	{question: "Did I join the Marines, Army, or Navy", ans: "m"},
+	{question: "I was born in Oregon, Pennsylvania, or California", ans: "c"},
+	{question: "I prefer Hiking over Biking", ans: "y"},
+	{question: "I prefer to take Trains over Airplanes", ans: "n"},
+	{question: "I am allergic to bees", ans: "n"},
+];
